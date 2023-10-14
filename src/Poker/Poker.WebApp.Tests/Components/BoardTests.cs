fb4cea5e86abab9f20ea9429a2ca674ad3d94bbd
Alex Hedley <alex.hedley@me.com>
2023-10-14 13:32:45 +0100

    Setup initial components (and tests)

---
+++ src/Poker/Poker.WebApp.Tests/Components/BoardTests.cs
@@ -0,0 +1,21 @@
+﻿using Bunit;
+using Poker.WebApp.Components.Board;
+
+namespace Poker.WebApp.Tests.Components
+{
+    public class BoardTests : TestContext
+    {
+        public BoardTests() { }
+
+        [Fact]
+        public void BoardShows()
+        {
+            var cut = RenderComponent<BoardComponent>();
+            string markup = "<h3>BoardComponent</h3>";
+            cut.FindAll("h3")[0].MarkupMatches(markup);
+
+            string markup2 = "<p>C1 | C2 | C3 | C4 | C5</p>";
+            cut.FindAll("p")[0].MarkupMatches(markup2);
+        }
+    }
+}
