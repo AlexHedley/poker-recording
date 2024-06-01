c67feae5f939e5e8a83c174287a822b28a17afba
Alex Hedley <alex.hedley@me.com>
2024-06-01 13:33:59 +0100

    WIP HUD for Stats

---
+++ src/Poker/Poker.Server.WebApp/Pages/Hud.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Poker.Server.WebApp.Pages;
+
+public partial class Hud
+{
+    #region Properties
+    #endregion Properties
+
+    protected override void OnInitialized() { }
+}
