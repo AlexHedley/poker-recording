ce9b0624b8164e7bac78f7c019784a5b69c9f278
Alex Hedley <alex.hedley@me.com>
2024-06-02 17:01:04 +0100

    Moving to shared Components

---
+++ src/Poker/Poker.Components/Board1.razor.cs
@@ -0,0 +1,9 @@
+namespace Poker.Components;
+public partial class Board1
+{
+    public string BoardFlopOne;
+    public string BoardFlopTwo;
+    public string BoardFlopThree;
+    public string BoardTurn;
+    public string BoardRiver;
+}
