7575bbccce99efc0cb94948248c40c9dd4adcd0f
Alex Hedley <alex.hedley@me.com>
2024-06-02 18:24:48 +0100

    Component: Player

---
+++ src/Poker/Poker.Components/Models/Player.cs
@@ -0,0 +1,25 @@
+﻿namespace Poker.Components.Models
+{
+    public class Player
+    {
+        public string? Name { get; set; }
+
+        public string? Picture { get; set; }
+
+        public string? Flag { get; set; }
+
+        public string? Card1 { get; set; }
+
+        public string? Card2 { get; set; }
+
+        public string? PotOdds { get; set; }
+
+        public string? Move { get; set; }
+
+        public string? Unknown { get; set;}
+
+        public string? Chips { get; set; }
+
+        public string? Info { get; set; }
+    }
+}
---
+++ src/Poker/Poker.Components/Player1.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+using Poker.Components.Models;
+
+namespace Poker.Components;
+public partial class Player1
+{
+    [Parameter]
+    public Player? Player { get; set; }
+
+    //protected override void OnInitialized() { }
+}
