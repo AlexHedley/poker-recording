ba39cea78d7ff10718be5b3a2acd36ff72084e77
Alex Hedley <alex.hedley@me.com>
2024-06-02 19:42:16 +0100

    Add a second player to the HUD and refactor

--- src/Poker/Poker.WebApp/Pages/Hud.razor.cs
+++ src/Poker/Poker.WebApp/Pages/Hud.razor.cs
@@ -3,26 +3,43 @@ using Poker.Components.Models;
 namespace Poker.WebApp.Pages;
 public partial class Hud
 {
-    public Player? Player;
+    public Player? Player1;
+    public Player? Player2;
 
     //protected override async Task OnInitializedAsync() {}
     protected override void OnInitialized()
     {
-        SetupPlayer();
+        SetupPlayer1();
+        SetupPlayer2();
     }
 
-    void SetupPlayer()
+    void SetupPlayer1()
     {
-        Player = new Player();
-        Player.Card1 = "_content/Poker.Components/images/cards/12-spade.png";
-        Player.Card2 = "_content/Poker.Components/images/cards/11-spade.png";
-        Player.Name = "Gareth";
-        Player.Picture = "_content/Poker.Components/images/players/man1.svg";
-        Player.PotOdds = "100%";
-        Player.Move = "Check";
-        Player.Unknown = "SB";
-        Player.Chips = "869k";
-        Player.Info = "Bluff";
-        Player.Flag = "_content/Poker.Components/images/flags/us.jpg";
+        Player1 = new Player();
+        Player1.Card1 = "_content/Poker.Components/images/cards/12-spade.png";
+        Player1.Card2 = "_content/Poker.Components/images/cards/11-spade.png";
+        Player1.Name = "Gareth";
+        Player1.Picture = "_content/Poker.Components/images/players/man1.svg";
+        Player1.PotOdds = "100%";
+        Player1.Move = "Check";
+        Player1.Unknown = "SB";
+        Player1.Chips = "869k";
+        Player1.Info = "";
+        Player1.Flag = "_content/Poker.Components/images/flags/uk.jpg";
+    }
+
+    void SetupPlayer2()
+    {
+        Player2 = new Player();
+        Player2.Card1 = "_content/Poker.Components/images/cards/8-spade.png";
+        Player2.Card2 = "_content/Poker.Components/images/cards/6-spade.png";
+        Player2.Name = "Phil";
+        Player2.Picture = "_content/Poker.Components/images/players/man2.svg";
+        Player2.PotOdds = "0%";
+        Player2.Move = "";
+        Player2.Unknown = "+1";
+        Player2.Chips = "974k";
+        Player2.Info = "Bluff";
+        Player2.Flag = "_content/Poker.Components/images/flags/us.jpg";
     }
 }
