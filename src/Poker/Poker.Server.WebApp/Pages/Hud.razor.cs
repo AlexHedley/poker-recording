456ba202078795635a6c41cebe5931d66efee7fb
Alex Hedley <alex.hedley@me.com>
2024-06-02 18:39:03 +0100

    Poker Server WebApp updates

--- src/Poker/Poker.Server.WebApp/Pages/Hud.razor.cs
+++ src/Poker/Poker.Server.WebApp/Pages/Hud.razor.cs
@@ -1,12 +1,30 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Poker.Components.Models;
 
 namespace Poker.Server.WebApp.Pages;
 
 public partial class Hud
 {
-    #region Properties
-    #endregion Properties
+    public Player? Player;
 
-    protected override void OnInitialized() { }
+    //protected override async Task OnInitializedAsync() {}
+
+    protected override void OnInitialized()
+    {
+        SetupPlayer();
+    }
+
+    void SetupPlayer()
+    {
+        Player = new Player();
+        Player.Card1 = "_content/Poker.Components/images/cards/12-spade.png";
+        Player.Card2 = "_content/Poker.Components/images/cards/11-spade.png";
+        Player.Name = "Gareth";
+        Player.Picture = "_content/Poker.Components/images/players/man1.svg";
+        Player.PotOdds = "100%";
+        Player.Move = "Check";
+        Player.Unknown = "SB";
+        Player.Chips = "869k";
+        Player.Info = "Bluff";
+        Player.Flag = "_content/Poker.Components/images/flags/us.jpg";
+    }
 }
