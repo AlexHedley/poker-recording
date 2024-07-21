9b7a2ee00cd467f23fb7b70181437cb04c14281b
Alex Hedley <alex.hedley@me.com>
2024-07-21 16:22:39 +0100

    #55 WIP FluentUI Wizard

--- docs/ACKNOWLEDGEMENTS.md
+++ docs/ACKNOWLEDGEMENTS.md
@@ -1,6 +1,9 @@
 # Acknowledgements
 
-- [cardmeister](https://cardmeister.github.io/)
-  - [code](https://github.com/cardmeister/cardmeister.github.io)
+-   [cardmeister](https://cardmeister.github.io/)
 
-- [favicon](https://www.favicon.cc/?action=icon&file_id=330076)
+    -   [code](https://github.com/cardmeister/cardmeister.github.io)
+
+-   [favicon](https://www.favicon.cc/?action=icon&file_id=330076)
+
+-   https://github.com/microsoft/fluentui-blazor
---
+++ src/Poker/Poker.WebApp/Pages/Night.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace Poker.WebApp.Pages;
+public partial class Night
+{
+    [Inject]
+    public required IDialogService DialogService { get; init; }
+
+    WizardStepSequence StepSequence = WizardStepSequence.Linear;
+    string Player1 = "Alex";
+    string Time;
+    string BuyIn;
+
+    void OnStepChange(FluentWizardStepChangeEventArgs e)
+    {
+        Console.WriteLine($"Go to step {e.TargetLabel} (#{e.TargetIndex})");
+    }
+
+    async Task OnFinishedAsync()
+    {
+        await DialogService.ShowInfoAsync("Enjoy your game 🃏");
+    }
+}
